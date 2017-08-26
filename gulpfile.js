f82e744d4fe8e2fb402e4e672ad5d315165dc235
Tugrul Elmas <tugrulelmas@Tugruls-MacBook-Pro.local>
2017-08-27 02:08:17 +0300

    dockerizing

--- gulpfile.js
+++ gulpfile.js
@@ -18,22 +18,13 @@ var banner = ['/*!\n',
 ].join('');
 
 // Default task
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-js']);
 
 // Less task to compile the less files and add the banner
 gulp.task('less', function() {
     return gulp.src('less/agency.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-// Minify CSS
-gulp.task('minify-css', function() {
-    return gulp.src('css/agency.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
@@ -53,34 +44,6 @@ gulp.task('minify-js', function() {
             stream: true
         }))
 });
-
-// Copy Bootstrap core files from node_modules to vendor directory
-gulp.task('bootstrap', function() {
-    return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-})
-
-// Copy jQuery core files from node_modules to vendor directory
-gulp.task('jquery', function() {
-    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-})
-
-// Copy Font Awesome core files from node_modules to vendor directory
-gulp.task('fontawesome', function() {
-    return gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
-
-// Copy all third party dependencies from node_modules to vendor directory
-gulp.task('copy', ['bootstrap', 'jquery', 'fontawesome']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
