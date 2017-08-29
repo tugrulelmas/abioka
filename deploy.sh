424876b4ea569630a19d0d08f208e1343221158a
Tuğrul Elmas <tugrulelmas@gmail.com>
2017-08-29 20:03:45 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo param value = $1
-ssh -v -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
+ssh -v -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com "env TAG=$1" <<-'ENDSSH'
   docker system prune -af
-  env ABIOKA_TAG=$1 docker stack deploy -c docker-compose.yml abioka
+  env ABIOKA_TAG=${TAG} docker stack deploy -c docker-compose.yml abioka
   docker images
   docker ps
   docker service ls
