77b317455ca0efbd9130ad5c069023004da6f9c1
Tuğrul Elmas <tugrulelmas@gmail.com>
2017-08-29 15:03:21 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 ssh -v -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
   docker system prune -af
   docker stack rm abioka
-  docker service rm abioka_app
   docker stack deploy -c docker-compose.yml abioka
   docker images
   docker ps
