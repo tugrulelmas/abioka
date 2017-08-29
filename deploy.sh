3b29f9909868c8bca3e13d3c066cc8b524882474
Tuğrul Elmas <tugrulelmas@gmail.com>
2017-08-29 11:07:44 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 ssh -v -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
   ls
-  docker stack rm abioka
   docker stack deploy -c docker-compose.yml abioka
   docker images
   docker ps
+  docker service ls
+  docker service update --image tugrulelmas/abioka-com:latest abioka_app
 ENDSSH
