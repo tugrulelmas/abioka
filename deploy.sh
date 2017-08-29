78d13383b1c63adb6986a382e7140edf45d1d783
Tuğrul Elmas <tugrulelmas@gmail.com>
2017-08-29 09:04:56 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ssh -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
-  scp -P docker-compose.yml ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com:/home/ec2-user
+  scp docker-compose.yml ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com:/home/ec2-user
   ls
   docker stack rm abioka
   docker stack deploy -c docker-compose.yml abioka
