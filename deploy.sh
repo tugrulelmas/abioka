c61b34b78be2f46bd39f8d0e8442d1152ca997aa
Tuğrul Elmas <tugrulelmas@gmail.com>
2017-08-29 21:16:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ssh -v -o SendEnv TAG=$1 -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
+ssh -v -i "abioka-linux.pem" ec2-user@ec2-35-158-221-148.eu-central-1.compute.amazonaws.com <<-'ENDSSH'
   docker system prune -af
   env ABIOKA_TAG=latest docker stack deploy -c docker-compose.yml abioka
   docker images
